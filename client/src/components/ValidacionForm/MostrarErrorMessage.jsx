430e69c78dc95b62e95ea2d7c57f5bdfb5ecc4fa
luisiportal <luisiportal@gmail.com>
2024-04-27 16:48:02 -0400

    fgfg

---
+++ client/src/components/ValidacionForm/MostrarErrorMessage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MostrarErrorMessage = ({ errors, campo }) => {
+  return (
+    <div>
+      {errors[campo] && (
+        <span className="bg-red-500 p-1 m-1 rounded">{errors[campo]}</span>
+      )}
+    </div>
+  );
+};
+export default MostrarErrorMessage;
+
+import React from "react";
