ef2b6bcc30174fa0cda9778c7ef6c1bffa125253
luisiportal <luisiportal@gmail.com>
2024-10-12 23:18:19 -0400

    (actualizar backend)

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(
   cors({
-    origin: [FRONTEND_URL],
+    origin: [FRONTEND_URL, FRONTEND_URL2],
     credentials: true,
   })
 );
